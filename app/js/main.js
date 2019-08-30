87241ecd452bc9ed6f9b5adfa51acf277e76c7b5
Kiersten <kiersten.schmidt@nbcuniversal.com>
2019-08-30 11:02:15 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,7 @@ Highcharts.setOptions({
 document.addEventListener('DOMContentLoaded', function () {
     const myChart = Highcharts.chart('chart-container', {
         chart: {
-            type: 'bar',
+            type: 'column',
             styledMode: true,
             spacingBottom: 25,
             spacingRight: 100
@@ -31,60 +31,59 @@ document.addEventListener('DOMContentLoaded', function () {
         title: {
             text: null
         },
-        data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
-        },
+        series: [{
+            name: 'Airfare',
+            data: [
+                ['Monday, Nov. 25', 288],
+                ['Tuesday, Nov. 26', 321],
+                ['Wednesday, Nov. 27', 359],
+                ['Thursday, Nov. 28', 275]
+            ],
+        }],
         // for bar charts only
         plotOptions: {
             series: {
                 groupPadding: 0.1
             } 
         },
-        // for line charts only
-        // plotOptions: {
-        //     series: {
-        //         lineWidth: 1,
-        //         // clip: false,
-        //         marker: {
-        //             enabled: false,
-        //             symbol: 'circle',
-        //             fillColor: '#ffffff',
-        //             states: {
-        //                 hover: {
-        //                     fillColor: '#ffffff'
-        //                 }
-        //             }
-        //         }
-        //     }
-        // },
         legend: {
-            align: 'right',
-            symbolRadius: 0,
-            verticalAlign: 'top',
-            x: 10,
-            itemMarginTop: -10
+            enabled: false
         },
         xAxis: {
             labels: {
+                autoRotation: false,
                 style: {
-                    whiteSpace: 'nowrap'
+                    // whiteSpace: 'nowrap'
+                    textOverflow: 'none'
+                },
+                formatter: function() {
+                    if (this.value !== 'Monday, Nov. 25') {
+                        let justDate = (this.value).slice(-2);
+                        return justDate;
+                    } else {
+                        return 'Nov. 25';
+                    }
                 }
             },
-            tickLength: 5
+            tickLength: 5,
+            type: 'category'
         },
         yAxis: {
             title: false,
             labels: {
                 useHTML: true,
                 overflow: 'allow'
-            }
+            },
+            max: 600,
+            tickAmount: 4
         },
         credits: {
             enabled: false
         },
         tooltip: {
             shadow: false,
-            padding: 10
+            padding: 10,
+            valuePrefix: '$'
         },
         responsive: {
             rules: [{
@@ -93,14 +92,95 @@ document.addEventListener('DOMContentLoaded', function () {
               },
               chartOptions: {
                 chart: {
-                  spacingRight: 10
+                  spacingRight: 15
                 },
                 legend: {
                     align: 'left',
                     x: -18
+                }
+              }
+            }]
+        }
+    });
+
+    const myChart2 = Highcharts.chart('chart-container-2', {
+        chart: {
+            type: 'column',
+            styledMode: true,
+            spacingBottom: 25,
+            spacingRight: 100
+        }, 
+        title: {
+            text: null
+        },
+        series: [{
+            name: 'Airfare',
+            data: [
+                ['Friday, Dec. 20', 496],
+                ['Saturday, Dec. 21', 504],
+                ['Sunday, Dec. 22', 433],
+                ['Monday, Dec. 23', 394],
+                ['Tuesday, Dec. 24', 344],
+                ['Wednesday, Dec. 25', 358]
+            ],
+        }],
+        // for bar charts only
+        plotOptions: {
+            series: {
+                groupPadding: 0.1,
+            } 
+        },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                autoRotation: false,
+                style: {
+                    // whiteSpace: 'nowrap',
+                    textOverflow: 'none'
                 },
-                tooltip: {
-                    enabled: false
+                formatter: function() {
+                    if (this.value !== 'Friday, Dec. 20') {
+                        let justDate = (this.value).slice(-2);
+                        return justDate;
+                    } else {
+                        return 'Dec. 20';
+                    }
+                }
+            },
+            tickLength: 5,
+            type: 'category'
+        },
+        yAxis: {
+            title: false,
+            labels: {
+                useHTML: true,
+                overflow: 'allow'
+            },
+            max: 600,
+            tickAmount: 4
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            shadow: false,
+            padding: 10,
+            valuePrefix: '$'
+        },
+        responsive: {
+            rules: [{
+              condition: {
+                maxWidth: 500
+              },
+              chartOptions: {
+                chart: {
+                  spacingRight: 15
+                },
+                legend: {
+                    align: 'left',
+                    x: -18
                 }
               }
             }]
